502433f7f944a4d1cc967619dc21c384fd624ab7
alfandy123 <63333665+alfandy123@users.noreply.github.com>
2020-04-08 12:55:58 +0700

    Add files via upload
    tugas web 2 minggu ke-4
    Alfandy Heri Pratama
    A22.2019.02731

---
+++ praktikum & latihan modul 3/inputcheckbox.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Band Favorit ~ Inputan Checkbox</title></head> 
+<body> 
+<FORM ACTION="prosescheckbox.php" METHOD="POST" NAME="input"> 
+<h2>Pilih Band Favorit Anda :</h2> 
+     <input type="checkbox" name="band01" value="Padi" checked>
+Padi<br> 
+	 <input type="checkbox" name="band02" value="Sheila On7"> 
+Sheila On 7<br> 
+	 <input type="checkbox" name="band03" value="Dewa 19">Dewa 
+	 <input type="submit" name="Pilih" value="Pilih"> 
+</FORM> 
+</body>
+</html>
